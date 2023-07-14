52c408dee78b7cfb2fe38d5c0c48dd2c1aef6729
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-07-14 12:52:54 +0300

    assignment complete

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,8 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+  def sorry
+    john = Actor.find_or_create_by(first_name: "John", last_name: "Mulaney")
+    "We're sorry about passing on #{john.full_name}'s pilot"
+  end
 end
